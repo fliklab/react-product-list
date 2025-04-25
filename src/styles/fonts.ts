5f41fb72fd33e4294c5a8e2140a73c055b4a984c
Flik Jeong <jsh852@gmail.com>
2025-04-25 21:29:42 +0900

    Fix font file loading

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -7,7 +7,8 @@ export const fontFace = css`
     font-style: normal;
     font-display: swap;
     src: local("Pretendard Variable"),
-      url("/fonts/PretendardVariable.woff2") format("woff2-variations");
+      url("/fonts/PretendardVariable.woff2") format("woff2-variations"),
+      url("/fonts/PretendardVariable.woff") format("woff-variations");
   }
 `;
 
